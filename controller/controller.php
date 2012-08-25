235eb5e909846984ba4d18ed478f7e763c778a67
Fredrik Erlandsson <fredrik.e@gmail.com>
2012-08-25 14:33:55 +0200

    More stable and nicer controller

--- controller/controller.php
+++ controller/controller.php
@@ -7,10 +7,10 @@
 
 DROP TABLE IF EXISTS `page`;
 CREATE TABLE `page` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` varchar(255) default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=554 DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=745 DEFAULT CHARSET=latin1;
 
 --
 -- Table structure for table `post`
@@ -18,20 +18,43 @@ CREATE TABLE `page` (
 
 DROP TABLE IF EXISTS `post`;
 CREATE TABLE `post` (
-  `id` int(32) NOT NULL default '0',
-  `post_id` varchar(48) default NULL,
-  `page_id` int(11) default NULL,
-  `seq` int(11) default NULL,
-  `date` varchar(13) default NULL,
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `post_id` varchar(48) DEFAULT NULL,
+  `page_id` int(11) DEFAULT NULL,
+  `seq` int(11) DEFAULT NULL,
+  `date` varchar(13) DEFAULT NULL,
   `data` longblob,
-  `status` varchar(40) default NULL,
-  `time_stamp` int(11) default NULL,
-  `who` varchar(60) default NULL,
-  `time` float default NULL,
-  PRIMARY KEY  (`id`),
+  `status` varchar(40) DEFAULT NULL,
+  `time_stamp` int(11) DEFAULT NULL,
+  `who` varchar(60) DEFAULT NULL,
+  `time` float DEFAULT NULL,
+  PRIMARY KEY (`id`),
   UNIQUE KEY `post_id` (`post_id`),
-  KEY `page_id` (`page_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2427809 DEFAULT CHARSET=latin1;
+  KEY `page_id` (`page_id`),
+  KEY `IDX_FIELD2` (`post_id`),
+  KEY `status_time_stamp_id` (`status`,`time_stamp`,`id`),
+  KEY `status_post_id_time_stamp_id` (`status`,`post_id`,`time_stamp`,`id`),
+  KEY `id_status_timestamp` (`id`,`status`,`time_stamp`)
+) ENGINE=InnoDB AUTO_INCREMENT=3472802 DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `post_data`
+--
+
+DROP TABLE IF EXISTS `post_data`;
+CREATE TABLE `post_data` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `data` longblob,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3472802 DEFAULT CHARSET=latin1;
+--
+-- Table structure for table `pull_posts`
+--
+
+DROP TABLE IF EXISTS `pull_posts`;
+CREATE TABLE `pull_posts` (
+  `id` int(32) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
  *
  * Don't forget to set the default values below.
  */
@@ -42,6 +65,7 @@ define(_PDO_password_, 'password');
 
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
+ini_set('error_log','logFile.log');
 $action='';
 if(isset($_GET['action']))
   $action = $_GET['action'];
@@ -64,12 +88,12 @@ default:
 
 #Checkout file, add to db.
 function checkout() {
-  set_time_limit(120);
+  set_time_limit(300);
   $count =0;
   $dbh = new PDO(PDO_dsn, PDO_username, PDO_password);
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
   //Read file for now.
-  $files = glob('posts-files/posts.txt.*');
+  $files = glob('posts/posts.txt.*');
   //$postsFName = $file;
   foreach ($files as $postsFName){
     if (!(file_exists($postsFName) && $postsFilePtr = fopen($postsFName, "r")))
@@ -82,7 +106,7 @@ function checkout() {
       continue;
     }
     $result->closeCursor();
-
+    print "Adding $file <br/>\n"; flush(); ob_flush();
     //Create page row.
     $count++;
     $sql = "INSERT INTO page (name) VALUES (".$file.")";
@@ -97,9 +121,10 @@ function checkout() {
       fscanf($postsFilePtr, "%s\n", $currentPost);
       $postsCount++;
       $sth->execute(array($postsCount,$currentTime,$currentPost));
+      $sth->closeCursor();
     }
   }
-  print "$count rows added.\n<br/>";
+  print "$count rows added.<br/>\n";
 }
 
 function pull_post($count=3) {
@@ -147,12 +172,16 @@ function my_push() {
     $result = $db->query($sql);
     if(($row=$result->fetch())) {
       $sql = "UPDATE post SET status = 'done'".
-        ", data = ".$db->quote($post['data']).
+        ", who = ".$db->quote($_SERVER["REMOTE_ADDR"].":".$_SERVER["REMOTE_PORT"]).
         ", time = ".$db->quote($post['exec_time']).
         ", time_stamp = UNIX_TIMESTAMP()".
-        ", who = ".$db->quote($_SERVER["REMOTE_ADDR"].":".$_SERVER["REMOTE_PORT"]).
-        " WHERE id = '".$row['id']."'; ";
-      file_put_contents('done/raw/'.$row['fname'], gzinflate(substr(base64_decode($post['data']),10,-8)));
+        " WHERE id = ".$row['id']."; ";
+      $sql .= "INSERT INTO post_data VALUES (".$row['id'].
+          ", data = ".$db->quote($post['data']).
+          ") on duplicate key UPDATE ".
+          "data = ".$db->quote($post['data']).";";
+      file_put_contents('raw/'.$row['fname'], gzinflate(substr(base64_decode($post['data']),10,-8)));
+#      file_put_contents('statements.sql', "-- ".$row['fname']."\n".parseJsonString(gzinflate(substr(base64_decode($post['data']),10,-8))),FILE_APPEND);
     } else {
       die("No post with id $post_id in db");
     }
@@ -167,25 +196,116 @@ function my_push() {
 function my_list() {
 ?>
   <link rel="stylesheet" href="html/style.css" type="text/css" id="" media="print, projection, screen" />
-  <script type="text/javascript" src="html/jquery-latest.js"></script>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 
-  <script type="text/javascript" src="html/__jquery.tablesorter.js"></script>
+  <script type="text/javascript" src="html/jquery.tablesorter.min.js"></script>
+  <script type="text/javascript" src="html/jquery.tablesorter.widgets.min.js"></script>
+  <script type="text/javascript">
+  function myTime(timestamp) {
+  //function parses mysql datetime string and returns javascript Date object
+  //input has to be in this format: 2007-06-05 15:26:02
+  var regex=/^([0-9]{2,4})-([0-1][0-9])-([0-3][0-9]) (?:([0-2][0-9]):([0-5][0-9]):([0-5][0-9]))?$/;
+  var parts=timestamp.replace(regex,"$1 $2 $3 $4 $5 $6").split(' ');
+  return new Date(Date.UTC(parts[0],parts[1]-1,parts[2],parts[3],parts[4],parts[5])).getTime()/1000;
+  }
+  var serverTime = <? echo time(); ?>;
+  </script>
 <script type="text/javascript" id="js">$(document).ready(function() {
   // call the tablesorter plugin
   $("table").tablesorter({
+    widgets: ["zebra", "filter"],
+
+    widgetOptions : {
+
+      // css class applied to the table row containing the filters & the inputs within that row
+      filter_cssFilter : 'tablesorter-filter',
+
+      // If there are child rows in the table (rows with class name from "cssChildRow" option)
+      // and this option is true and a match is found anywhere in the child row, then it will make that row
+      // visible; default is false
+      filter_childRows : false,
+
+      // Set this option to true to use the filter to find text from the start of the column
+      // So typing in "a" will find "albert" but not "frank", both have a's; default is false
+      filter_startsWith : false,
+
+      // Set this option to false to make the searches case sensitive
+      filter_ignoreCase : true,
+
+      // Delay in milliseconds before the filter widget starts searching; This option prevents searching for
+      // every character while typing and should make searching large tables faster.
+      filter_searchDelay : 300,
+
+      // Add select box to 4th column (zero-based index)
+      // each option has an associated function that returns a boolean
+      // function variables:
+      // e = exact text from cell
+      // n = normalized value returned by the column parser
+      // f = search filter input value
+      // i = column index
+      filter_functions : {
+
+        // Add select menu to this column
+        // set the column value to true, and/or add "filter-select" class name to header
+        // 0 : true,
+        0 : {
+          "Running"      : function(e, n, f, i) { return n > 0 && n < 100; },
+          "Done" : function(e, n, f, i) { return n >= 99.9999; },
+          "New"     : function(e, n, f, i) { return n <= 0.001; }
+        },
+        2 : {
+          "last 20 min"      : function(e, n, f, i) { return serverTime-myTime(e) < 1200; },
+          "last 1 h" : function(e, n, f, i) { return serverTime-myTime(e) <= 3600; },
+          "last 12 h" : function(e, n, f, i) { return serverTime-myTime(e) <= 43200; },
+          "12 h - 24 h " : function(e, n, f, i) { return serverTime-myTime(e) >= 43200 && serverTime-myTime(e) <= 86400; },
+          "1 - 7 days" : function(e, n, f, i) { return serverTime-myTime(e) >= 86400 && serverTime-myTime(e) <= 604800; },
+          "7 - 14 days" : function(e, n, f, i) { return serverTime-myTime(e) >= 604800 && serverTime-myTime(e) <= 1209600; },
+          "> 14 days"     : function(e, n, f, i) { return serverTime-myTime(e) > 1209600; }
+        },
+        // Add these options to the select dropdown (regex example)
+        4 : {
+          "A - D" : function(e, n, f, i) { return /^[A-D]/.test(e); },
+          "E - H" : function(e, n, f, i) { return /^[E-H]/.test(e); },
+          "I - L" : function(e, n, f, i) { return /^[I-L]/.test(e); },
+          "M - P" : function(e, n, f, i) { return /^[M-P]/.test(e); },
+          "Q - T" : function(e, n, f, i) { return /^[Q-T]/.test(e); },
+          "U - X" : function(e, n, f, i) { return /^[U-X]/.test(e); },
+          "Y - Z" : function(e, n, f, i) { return /^[Y-Z]/.test(e); }
+        },
+
+        // Add these options to the select dropdown (numerical comparison example)
+        // Note that only the normalized (n) value will contain numerical data
+        // If you use the exact text, you'll need to parse it (parseFloat or parseInt)
+        3 : {
+          "< 1200s (20 min)"      : function(e, n, f, i) { return n < 1200; },
+          "20 min - 1 h" : function(e, n, f, i) { return n >= 1200 && n <= 3600; },
+          "1 h - 12 h" : function(e, n, f, i) { return n >= 3600 && n <= 43200; },
+          "12 h - 24 h " : function(e, n, f, i) { return n >= 43200 && n <= 86400; },
+          "1 - 7 days" : function(e, n, f, i) { return n >= 86400 && n <= 604800; },
+          "7 - 14 days" : function(e, n, f, i) { return n >= 604800 && n <= 1209600; },
+          "> 14 days"     : function(e, n, f, i) { return n > 1209600; }
+        }
+      }
+
+    },
+   initialized : function(table){
+     $('select:eq(1)').val($('select:eq(1)>*:eq(3)').val()).change();
+     },
+
     sortList: [[0,1],[2,0]]
   });
 }); </script>
 
   <?
+  flush();
   $db = new PDO(PDO_dsn, PDO_username, PDO_password);
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-  $db->setAttribute(PDO::ATTR_TIMEOUT, "120");
+  $db->setAttribute(PDO::ATTR_TIMEOUT, "30");
   print "<table id='myTable' class='tablesorter'>";
-  print "<thead><tr><th> Status (%)</th><th>Name</th><th>Time</th><th>Exec Time (s)</th><th>Status</th><th>Pulled</th></tr></thead>";
+  print "<thead><tr><th data-placeholder=\"--\">Status (%)</th><th>Name</th><th>Last modification time</th><th data-placeholder=\"--\">Exec Time (s)</th><th data-placeholder=\"try /\d+\\//\">Status</th><th>Pulled</th></tr></thead>";
   $sql_status = "SELECT ".
     "ROUND(COUNT(CASE WHEN status = 'done' THEN 1 END)*100.0/COUNT(*),4), ".
-    "name, FROM_UNIXTIME(time_stamp), ROUND(SUM(time), 4),".
+    "name, FROM_UNIXTIME(MAX(time_stamp)), ROUND(SUM(time), 4),".
     "CONCAT(COUNT(CASE WHEN status = 'done' THEN 1 END),'/', COUNT(*)) ,".
     "CONCAT(COUNT(CASE WHEN status = 'pulled' THEN 1 END) , '/',COUNT(*))".
     "FROM page JOIN post WHERE post.page_id=page.id GROUP BY page.id;";
--- controller/createArchive.sh
+++ controller/createArchive.sh
@@ -9,25 +9,64 @@ OUTPUT_DIR='./done'
 mkdir -p -- "$OUTPUT_DIR"
 cd $OUTPUT_DIR
 
+if [ -f "creatingArchive.log" ]; then
+  echo "Process already running."
+  exit 1
+fi
+
+trap cleanup 1 2 3 4 5 6 9
+
+cleanup()
+{
+  echo "Caught Signal ... cleaning up."
+  if [ -n "$ERROR_ID" ]; then
+    echo "UPDATE post SET status='pulled' WHERE id IN (${ERROR_ID%, })" | $SQL_CMD
+    echo "Errors occored in: $ERROR_ID"
+  fi
+  rm -f $FILE $TAR "creatingArchive.log"
+  exit 1
+}
+
+touch "creatingArchive.log"
+
 SQL="SELECT id,name FROM (SELECT page.id AS id,name, COUNT(CASE WHEN status = 'done' THEN 1.0 END)*100.0/COUNT(*) AS done from page join post where post.page_id=page.id group by page.id) AS T where done = 100;"
 
-for PAGE in `echo $SQL | $SQL_CMD | sed 's/\t/|/g'`; do
+for PAGE in `echo $SQL | $SQL_CMD | sed 's/	/|/g'`; do
+ #   echo $PAGE; continue
   TAR=${PAGE#*\|}
   if [ -f "$TAR.tgz" ]; then
-    echo "- $TAR.tgz already exist, skipping (to re-generage issue rm -f \"$TAR.tgz\")."
+#    echo "- [`date +'%D %T'`] $TAR.tgz already exist, skipping (to re-generage issue rm -f \"$TAR.tgz\")."
     continue
   fi
-  echo "+ Creating ${TAR}.tgz"
+  echo -n "+ [`date +'%D %T'`] Creating ${TAR}.tgz"
   touch "${TAR}"
+  SQL="SELECT CONCAT(page.name , '_' , substr(CONCAT('00000000',seq),-8,8) , '_' , substr(date,1,13),'_', post_id ,'.json')  from post join page on post.page_id=page.id WHERE page.id=${PAGE%\|*};"
+  echo $SQL | $SQL_CMD | tar -cf "${TAR}" -C "${JSON_DIR}" -T -
+  if [ $? -eq 0 ]; then
+    gzip -f -S .tgz "${TAR}"
+    echo "+ [`date +'%D %T'`] ${TAR}.tgz done."
+    continue
+  fi
+
+  TAR_CONT=`tar -tf "$TAR"`
   SQL="SELECT post.id,CONCAT(page.name , '_' , substr(CONCAT('00000000',seq),-8,8) , '_' , substr(date,1,13),'_', post_id ,'.json')  from post join page on post.page_id=page.id WHERE page.id=${PAGE%\|*};"
   ERROR_ID=""
   for RES in `echo $SQL | $SQL_CMD | sed 's/\t/|/g'`; do
     ID=${RES%\|*}
     FILE=${RES#*\|}
+#Test if tar contains the $FILE
+    echo $TAR_CONT | grep $FILE - && continue
+#Else do the normal costy operations..
+    if [ -f "${JSON_DIR}/${FILE}" ]; then
+      echo -n "."
+      tar -uf "${TAR}" -C "${JSON_DIR}" $FILE
+      continue
+    fi
     echo "SELECT data FROM post WHERE id=$ID LIMIT 1"| $SQL_CMD | base64 -d -i 2>/dev/null | gzip -d 2>/dev/null > $FILE
     if [ $? -ne 0 ]; then
       ERROR_ID="$ID, $ERROR_ID"
     else
+      echo -n "+"
       tar -uf "${TAR}" $FILE
     fi
     rm $FILE
@@ -35,7 +74,11 @@ for PAGE in `echo $SQL | $SQL_CMD | sed 's/\t/|/g'`; do
   if [ -n "$ERROR_ID" ]; then
     echo "UPDATE post SET status='pulled' WHERE id IN (${ERROR_ID%, })" | $SQL_CMD
     echo "Errors occored in: $ERROR_ID"
+    rm $FILE
   else
     gzip -f -S .tgz "${TAR}"
+    echo "+ [`date +'%D %T'`] ${TAR}.tgz done."
   fi
 done
+
+rm "creatingArchive.log"
