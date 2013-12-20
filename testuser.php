b280b7c4ee72252ff24a6f109914c2c881707f32
CC Lien <cclien@gmail.com>
2013-12-20 21:33:10 +0000

    Tool for extracting test user TOKENs

---
+++ testuser.php
@@ -0,0 +1,22 @@
+<?php
+// Get test users' access tokens of the app
+require_once "./config/config.php";
+require_once "./facebook-php/src/facebook.php";
+
+$facebook = new Facebook(array(
+    'appId' => APPID,
+    'secret' => APPSEC,
+));
+
+$access_token = $facebook->getAccessToken();
+$accounts = $facebook->api("/".APPID."/accounts/test-users?access_token=$access_token");
+
+$id=1;
+foreach($accounts['data'] as $account) {
+  $filename="TOKEN.$id";
+  print "Write access of test user #$id into $filename\n";
+  file_put_contents($filename, $account['access_token']);
+  $id++;
+}
+
+?>
