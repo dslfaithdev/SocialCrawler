c06909f9f69ad0fb80fb07803a08336e453a9f61
Fredrik Erlandsson <fredrik.e@gmail.com>
2016-02-25 13:13:42 +0100

    set memory limit to inf for the agent

--- agent.php
+++ agent.php
@@ -1,6 +1,6 @@
 <?php
 define("VERSION", 2.9);
-ini_set('memory_limit', '256M');
+ini_set('memory_limit', -1);
 require_once "./config/config.php";
 require_once "./include/outputHandler.php";
 require_once "./facebook-php/src/facebook.php";
