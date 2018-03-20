55ee4aff54e2e78a8a2b5c9391289f57264c1901
Fredrik Erlandsson <fredrik.e@gmail.com>
2018-03-20 08:51:39 +0000

    add from tag in agent

--- agent.php
+++ agent.php
@@ -201,7 +201,7 @@ function crawl($currentPost, $facebook) {
   $fields = '?fields=message,type,picture,story,link,name,description,caption,icon,' .
     'created_time,updated_time,shares,' .
     'object_id,status_type,source,application,place,' .
-    'message_tags,to,with_tags,' .
+    'message_tags,to,from,with_tags,' .
     'likes.limit(1).summary(total_count){id},'.
     'comments.limit(1).summary(total_count){id}';
   $curr_feed = facebook_api_wrapper($facebook, '/' . $currentPost . $fields);
